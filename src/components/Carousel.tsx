e49ad70f91af380ed35655f7b34d3e8ce14b3028
BonyRoy <94554135+BonyRoy@users.noreply.github.com>
2024-04-20 21:17:58 +0530

    Update Carousel.tsx

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -63,11 +63,11 @@ const Carousel = () => {
                     </div>
                 ))}
             </div>
-            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev" onClick={goToPrevSlide} style={{backgroundColor:'#CEE8F2'}}>
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev" onClick={goToPrevSlide} >
                 <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span className="visually-hidden">Previous</span>
             </button>
-            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next" onClick={goToNextSlide} style={{backgroundColor:'#CEE8F2'}}>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next" onClick={goToNextSlide} >
                 <span className="carousel-control-next-icon" aria-hidden="true"></span>
                 <span className="visually-hidden">Next</span>
             </button>
