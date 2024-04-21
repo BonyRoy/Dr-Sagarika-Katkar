3e26e1635ca126ea823372a39321d93982b5552d
BonyRoy <94554135+BonyRoy@users.noreply.github.com>
2024-04-21 11:49:44 +0530

    Update Services.tsx

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -6,21 +6,24 @@ const Services = () => {
   return (
     <>
       <div className="container-fluid mb-5">
+        <br />
         <h2 className="text-center" style={{ color: '#D9B471' }}>Our Services</h2>
       </div>
-      <div className="container-fluid mb-5">
+      <div className="container mb-5">
         <div className="row justify-content-center">
-          <div className="col-10">
-            <div className="row" style={{display:'flex', alignItems:'center', justifyContent:'center'}}>
-              {Array.isArray(ServicesData) && ServicesData.map((val: any, ind: any) => {
-                return <Card key={ind} imgsrc={val.imgsrc} title={val.title} text={val.text} Link={val.Link} />
-              })}
+          <div className="col-lg-10">
+            <div className="row">
+              {Array.isArray(ServicesData) && ServicesData.map((val: any, ind: any) => (
+                <div key={ind} className="col-md-6 col-lg-4 mb-4">
+                  <Card imgsrc={val.imgsrc} title={val.title} text={val.text} Link={val.Link} />
+                </div>
+              ))}
             </div>
           </div>
         </div>
       </div>
     </>
-  )
+  );
 }
 
 export default Services;
